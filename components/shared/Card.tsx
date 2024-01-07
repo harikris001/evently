a0dca52af63ba6815759983c3c3ab6e7da7dcafd
harikris001 <hk5983835@gmail.com>
2024-01-07 18:41:13 +0530

    stripe payment

--- components/shared/Card.tsx
+++ components/shared/Card.tsx
@@ -42,7 +42,7 @@ const Card = ({ event, hasOrderLink, hidePrice }: CardProps) => {
       > 
        {!hidePrice && <div className="flex gap-2">
           <span className="p-semibold-14 w-min rounded-full bg-green-100 px-4 py-1 text-green-60">
-            {event.isFree ? 'FREE' : `$${event.price}`}
+            {event.isFree ? 'FREE' : `₹${event.price}`}
           </span>
           <p className="p-semibold-14 w-min rounded-full bg-grey-500/10 px-4 py-1 text-grey-500 line-clamp-1">
             {event.category.name}
