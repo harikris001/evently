87c80850117cebf46792d58d3e25be7a575f1028
harikris001 <hk5983835@gmail.com>
2024-01-07 18:19:37 +0530

    minor adjustments

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -45,7 +45,7 @@ const Home = async ({ searchParams }: SearchParamProps) => {
       </section> 
 
       <section id="events" className="wrapper my-8 flex flex-col gap-8 md:gap-12">
-        <h2 className="h2-bold">Trust by <br /> Thousands of Events</h2>
+        <h2 className="h2-bold">Trusted by <br /> Thousands of Event Organizers</h2>
         <div className="flex w-full flex-col gap-5 md:flex-row">
           <Search />
           <CategoryFilter />
--- components/shared/EventForm.tsx
+++ components/shared/EventForm.tsx
@@ -209,7 +209,7 @@ const EventForm = ({ userId, type, event, eventId }: EventFormProps) => {
                         onChange={(date: Date) => field.onChange(date)} 
                         showTimeSelect
                         timeInputLabel="Time:"
-                        dateFormat="MM/dd/yyyy h:mm aa"
+                        dateFormat="dd/MM/yyyy h:mm aa"
                         wrapperClassName="datePicker"
                       />
                     </div>
